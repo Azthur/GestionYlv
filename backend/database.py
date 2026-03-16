bfde5f6176f0984530af2c65195c3a831d7aa393
Juber Leon <juberleon28@gmail.com>
2026-03-16 13:34:16 -0500

    Dokerizado

--- backend/database.py
+++ backend/database.py
@@ -8,19 +8,22 @@ DB_SERVER = os.getenv("DB_SERVER")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+ODBC_DRIVER = os.getenv("ODBC_DRIVER", "{SQL Server}")
 
 def get_db_connection():
     try:
         conn_str = (
-            "DRIVER={SQL Server};"
+            f"DRIVER={ODBC_DRIVER};"
             f"SERVER={DB_SERVER};"
             f"DATABASE={DB_NAME};"
             f"UID={DB_USER};"
             f"PWD={DB_PASSWORD};"
             "TrustServerCertificate=yes;"
+            "Encrypt=no;"
         )
         conn = pyodbc.connect(conn_str)
         return conn
     except Exception as e:
         print(f"Error conectando a la base de datos: {e}")
         return None
+
